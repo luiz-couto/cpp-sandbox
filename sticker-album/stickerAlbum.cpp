989e51e421c4eaa33f8ccaf7c7934b2f36768cf6
luiz-couto <luizcouto.cg@gmail.com>
2025-10-14 15:45:53 +0100

    sticker album with more players

--- sticker-album/stickerAlbum.cpp
+++ sticker-album/stickerAlbum.cpp
@@ -4,6 +4,7 @@
 #define TOTAL_STICKERS 682
 #define STICKERS_PER_PACK 5
 #define PACK_PRICE 0.8
+#define NUMBER_OF_PLAYERS 10
 
 int generateRandomSticker(int seedHelper) {
     std::random_device rd;
@@ -30,21 +31,60 @@ bool isAlbumComplete(int album[TOTAL_STICKERS]) {
     return true;
 }
 
+bool isAnyAlbumComplete(int albums[NUMBER_OF_PLAYERS][TOTAL_STICKERS]) {
+    for (int p=0; p<NUMBER_OF_PLAYERS; p++) {
+        if (isAlbumComplete(albums[p])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void giveRepeatedStickerToAnotherPlayer(int currPlayer, int stickerNum, int albums[NUMBER_OF_PLAYERS][TOTAL_STICKERS]) {
+    bool matchFound = false;
+    for (int p=0; p<NUMBER_OF_PLAYERS; p++) {
+        if (p == currPlayer) continue;
+        int* albumPlayer = albums[p];
+        
+        for (int i=0; i<TOTAL_STICKERS; i++) {
+            if (albumPlayer[stickerNum] == 0) {
+                albumPlayer[stickerNum] = 1;
+                matchFound = true;
+                break;
+            }
+        }
+
+        if (matchFound) break;
+    }
+}
+
 int main() {
-    int album[TOTAL_STICKERS] = {};
-    int numberOfOpenedPacks = 0;
+    int albums[NUMBER_OF_PLAYERS][TOTAL_STICKERS] = {};
+    int numberOfOpenedPacksPerPlayer = 0;
+    bool completed = false;
 
-    while(!isAlbumComplete(album)) {
-        int* pack = openPack();
-        numberOfOpenedPacks++;
+    while(!completed) {
+        numberOfOpenedPacksPerPlayer++;
+        for (int p=0; p<NUMBER_OF_PLAYERS; p++) {
+            int* album = albums[p];
+            int* pack = openPack();
 
-        for (int i=0; i<STICKERS_PER_PACK; i++) {
-            album[pack[i]] = 1;
+            for (int i=0; i<STICKERS_PER_PACK; i++) {
+                if (album[pack[i]] == 1) {
+                   giveRepeatedStickerToAnotherPlayer(p, pack[i], albums);
+                }
+                album[pack[i]] = 1;
+            }
+
+            if (isAnyAlbumComplete(albums)) {
+                completed = true;
+                break;
+            }
         }
     }
 
-    float totalCost = numberOfOpenedPacks * PACK_PRICE;
+    float totalCost = numberOfOpenedPacksPerPlayer * PACK_PRICE;
     
-    std::cout << numberOfOpenedPacks << " packs were open\n";
+    std::cout << numberOfOpenedPacksPerPlayer << " packs were open\n";
     std::cout << "The total cost was " << totalCost << " pounds\n";
 }
