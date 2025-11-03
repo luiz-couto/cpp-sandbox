b373b367f1341a49e7ee8a060eab6f5e562829e2
luiz-couto <luizcouto.cg@gmail.com>
2025-11-03 11:39:28 +0000

    stl vectors

---
+++ stl-vectors/vector.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <print>
+
+int main() {
+    std::vector<float> vec = {0, 1 , 2, 3};
+
+    for (float &n : vec) {
+        n++;
+    }
+
+    for (float n : vec) {
+        std::print("{} - ", n);
+    }
+}
