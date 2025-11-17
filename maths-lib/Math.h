1a070251565ba7d6552816b0a4a73123d30655a4
luiz-couto <luizcouto.cg@gmail.com>
2025-11-17 10:43:11 +0000

    vec4

--- maths-lib/Math.h
+++ maths-lib/Math.h
@@ -8,12 +8,11 @@
 #define SQ(x) ((x) * (x))
 
 template<typename T>
-static T lerp(const T a, const T b, float t){
+static T lerp(const T a, const T b, float t) {
     return a * (1.0f - t) + (b * t);
 }
 
-class Vec3
-{
+class Vec3 {
     public:
     union
     {
@@ -177,4 +176,168 @@ template <>
 struct std::formatter<Vec3> : std::formatter<float> {
 auto format(const Vec3& v, auto& ctx) const {
     return std::format_to(ctx.out(), "Vec3[{}, {}, {}]", v.x, v.y, v.z);
+}};
+
+class Vec4 {
+    public:
+    union
+    {
+        float v[4];
+        struct { float x, y, z, w; };
+    };
+
+    Vec4() : x(0), y(0), z(0), w(0) {}
+    Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+    Vec4 operator+(const Vec4 &pVec) const {
+        return Vec4(v[0] + pVec.v[0], v[1] + pVec.v[1], v[2] + pVec.v[2], v[3] + pVec.v[3]); 
+    }
+
+    Vec4 operator+(const float scalar) const {
+        return Vec4(v[0] + scalar, v[1] + scalar, v[2] + scalar, v[3] + scalar); 
+    }
+
+    Vec4& operator+=(const Vec4& pVec) {
+        v[0] += pVec.v[0];
+        v[1] += pVec.v[1];
+        v[2] += pVec.v[2];
+        v[3] += pVec.v[3];
+        return *this;
+    }
+
+    Vec4& operator+=(const float scalar) {
+        v[0] += scalar;
+        v[1] += scalar;
+        v[2] += scalar;
+        v[3] += scalar;
+        return *this;
+    }
+
+    Vec4 operator*(const Vec4 &pVec) const {
+        return Vec4(v[0] * pVec.v[0], v[1] * pVec.v[1], v[2] * pVec.v[2], v[3] * pVec.v[3]); 
+    }
+
+    Vec4 operator*(const float scalar) const {
+        return Vec4(v[0] * scalar, v[1] * scalar, v[2] * scalar, v[3] * scalar); 
+    }
+
+    Vec4& operator*=(const Vec4& pVec) {
+        v[0] *= pVec.v[0];
+        v[1] *= pVec.v[1];
+        v[2] *= pVec.v[2];
+        v[3] *= pVec.v[3];
+        return *this;
+    }
+
+    Vec4& operator*=(const float scalar) {
+        v[0] *= scalar;
+        v[1] *= scalar;
+        v[2] *= scalar;
+        v[3] *= scalar;
+        return *this;
+    }
+
+    Vec4 operator/(const Vec4 &pVec) const {
+        return Vec4(v[0] / pVec.v[0], v[1] / pVec.v[1], v[2] / pVec.v[2], v[3] / pVec.v[3]); 
+    }
+
+    Vec4 operator/(const float scalar) const {
+        return Vec4(v[0] / scalar, v[1] / scalar, v[2] / scalar, v[3] / scalar); 
+    }
+
+    Vec4& operator/=(const Vec4& pVec) {
+        v[0] /= pVec.v[0];
+        v[1] /= pVec.v[1];
+        v[2] /= pVec.v[2];
+        v[3] /= pVec.v[3];
+        return *this;
+    }
+
+    Vec4& operator/=(const float scalar) {
+        v[0] /= scalar;
+        v[1] /= scalar;
+        v[2] /= scalar;
+        v[3] /= scalar;
+        return *this;
+    }
+
+    Vec4 operator-(const Vec4 &pVec) const {
+        return Vec4(v[0] - pVec.v[0], v[1] - pVec.v[1], v[2] - pVec.v[2], v[3] - pVec.v[3]); 
+    }
+
+    Vec4 operator-(const float scalar) const {
+        return Vec4(v[0] - scalar, v[1] - scalar, v[2] - scalar, v[3] - scalar); 
+    }
+
+    Vec4& operator-=(const Vec4& pVec) {
+        v[0] -= pVec.v[0];
+        v[1] -= pVec.v[1];
+        v[2] -= pVec.v[2];
+        v[3] -= pVec.v[3];
+        return *this;
+    }
+
+    Vec4& operator-=(const float scalar) {
+        v[0] -= scalar;
+        v[1] -= scalar;
+        v[2] -= scalar;
+        v[3] -= scalar;
+        return *this;
+    }
+
+    Vec4 operator-() const {
+        return Vec4(-v[0], -v[1], -v[2], -v[3]);
+    }
+
+    float length() const {
+        return std::sqrt(SQ(x) + SQ(y) + SQ(z) + SQ(w));
+    }
+
+    float lengthSquared() const {
+        return SQ(x) + SQ(y) + SQ(z) + SQ(w);
+    }
+
+    Vec4 normalize(void) {
+        float len = 1.0f / sqrtf(SQ(x) + SQ(y) + SQ(z) + SQ(w)); 
+        return Vec4(x * len, y * len, z * len, w * len);
+    }
+
+    float normalize_GetLength() {
+        float length = sqrtf(SQ(x) + SQ(y) + SQ(z) + SQ(w)); 
+        float len = 1.0f / length;
+        v[0] *= len; v[1] *= len; v[2] *= len; v[3] *= len;
+        return length;
+    }
+
+    float Max() const {
+        return std::max(std::max(x, y), std::max(z, w));
+    }
+
+    float Min() const {
+        return std::min(std::min(x, y), std::min(z, w));
+    }
+};
+
+Vec4 Max(const Vec4 &v1, const Vec4 &v2) {
+    return Vec4(
+        std::max(v1.x, v2.x),
+        std::max(v1.y, v2.y),
+        std::max(v1.z, v2.z),
+        std::max(v1.w, v2.w)
+    );
+};
+
+Vec4 Min(const Vec4 &v1, const Vec4 &v2) {
+    return Vec4(
+        std::min(v1.x, v2.x),
+        std::min(v1.y, v2.y),
+        std::min(v1.z, v2.z),
+        std::min(v1.w, v2.w)
+    );
+};
+
+template <>
+struct std::formatter<Vec4> : std::formatter<float> {
+auto format(const Vec4& v, auto& ctx) const {
+    return std::format_to(ctx.out(), "Vec4[{}, {}, {}, {}]", v.x, v.y, v.z, v.w);
 }};
