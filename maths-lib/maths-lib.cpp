02977e54819eec6c32fce42ad0ab70124d8522e3
luiz-couto <luizcouto.cg@gmail.com>
2025-11-23 12:01:41 +0000

    centralizing bunny

--- maths-lib/maths-lib.cpp
+++ maths-lib/maths-lib.cpp
@@ -37,7 +37,7 @@ class Camera {
     Vec3 to;
     Vec3 up;
 
-    Camera() : from(0, 0, 0.5f), to(0, 0, 0), up(0, -1, 0) {}
+    Camera() : from(0, 0, 0.5f), to(0, 0.05f, 0), up(0, -1, 0) {}
     Camera(Vec3 from, Vec3 to, Vec3 up) : from(from), to(to), up(up) {}
 };
 
