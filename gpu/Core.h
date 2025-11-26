b344745cecee889b855eca2f60b38e9b6349fdf4
luiz-couto <luizcouto.cg@gmail.com>
2025-11-26 10:03:11 +0000

    Math, Mesh and ScreenSpaceTriangle

--- gpu/Core.h
+++ gpu/Core.h
@@ -254,4 +254,50 @@ public:
         swapchain->Present(1, 0);
     }
 
+    // not so efficient. Better use copying queues, async work etc
+    void uploadResource(ID3D12Resource* dstResource, const void* data, unsigned int size, D3D12_RESOURCE_STATES targetState, D3D12_PLACED_SUBRESOURCE_FOOTPRINT *texFootprint = NULL) {
+        ID3D12Resource* uploadBuffer;
+        D3D12_HEAP_PROPERTIES heapProps = {};
+        heapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+        D3D12_RESOURCE_DESC bufferDesc = {};
+        bufferDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+        bufferDesc.Width = size;
+        bufferDesc.Height = 1; // we don't care about the height
+        bufferDesc.DepthOrArraySize = 1;
+        bufferDesc.MipLevels = 1;
+        bufferDesc.SampleDesc.Count = 1;
+        bufferDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+        device->CreateCommittedResource(&heapProps, D3D12_HEAP_FLAG_NONE, &bufferDesc,
+        D3D12_RESOURCE_STATE_GENERIC_READ, NULL, IID_PPV_ARGS(&uploadBuffer));
+
+        void* mappeddata = NULL;
+        uploadBuffer->Map(0, NULL, &mappeddata); // Map makes the buffer CPU accessible
+        memcpy(mappeddata, data, size);
+        uploadBuffer->Unmap(0, NULL); // unmap when we finish copying data - we finished with the CPU pointer
+        
+        // Allocate commands to copy
+        resetCommandList(); // reset the command list to record copy commands
+
+        if (texFootprint != NULL) {
+            D3D12_TEXTURE_COPY_LOCATION src = {};
+            src.pResource = uploadBuffer;
+            src.Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
+            src.PlacedFootprint = *texFootprint;
+            D3D12_TEXTURE_COPY_LOCATION dst = {};
+            dst.pResource = dstResource;
+            dst.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
+            dst.SubresourceIndex = 0;
+            getCommandList()->CopyTextureRegion(&dst, 0, 0, 0, &src, NULL);
+        } else {
+            // if it isn't a texture, just do a buffer copy
+            getCommandList()->CopyBufferRegion(dstResource, 0, uploadBuffer, 0, size);
+        }
+
+        Barrier::add(dstResource, D3D12_RESOURCE_STATE_COPY_DEST, targetState, getCommandList()); // prevent resource to be used before copy is finished
+
+        runCommandList(); // execute the copy commands
+        flushGraphicsQueue(); // wait until copy is finished. This is inefficient. In practice use multiple command queues and async work
+        uploadBuffer->Release();
+    }
+
 };
--- gpu/gpu.cpp
+++ gpu/gpu.cpp
@@ -1,5 +1,79 @@
 #include "Window.h"
 #include "Core.h"
+#include "Math.h"
+
+// Refactor to a mesh class later
+struct PRIM_VERTEX {
+    Vec3 position;
+    Colour colour;
+};
+
+class Mesh {
+    public:
+    ID3D12Resource* vertexBuffer;
+    D3D12_VERTEX_BUFFER_VIEW vbView;
+
+    Mesh(){}
+
+    void init(Core* core, void* vertices, int vertexSizeInBytes, int numVertices) {
+        D3D12_HEAP_PROPERTIES heapprops = {};
+        heapprops.Type = D3D12_HEAP_TYPE_DEFAULT;
+        heapprops.CreationNodeMask = 1;
+        heapprops.VisibleNodeMask = 1;
+
+        D3D12_RESOURCE_DESC vbDesc = {};
+        vbDesc.Width = numVertices * vertexSizeInBytes;
+        vbDesc.Height = 1;
+        vbDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+        vbDesc.DepthOrArraySize = 1;
+        vbDesc.MipLevels = 1;
+        vbDesc.SampleDesc.Count = 1;
+        vbDesc.SampleDesc.Quality = 0;
+        vbDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+        core->device->CreateCommittedResource(&heapprops, D3D12_HEAP_FLAG_NONE, &vbDesc,
+        D3D12_RESOURCE_STATE_COMMON, NULL, IID_PPV_ARGS(&vertexBuffer));
+
+        core->uploadResource(vertexBuffer, vertices, numVertices * vertexSizeInBytes, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+        
+        vbView.BufferLocation = vertexBuffer->GetGPUVirtualAddress(); // point where the buffer is located in GPU memory
+        vbView.StrideInBytes = vertexSizeInBytes; // size of each vertex. Everytime we step forward in the buffer, we move by this size
+        vbView.SizeInBytes = numVertices * vertexSizeInBytes;
+
+    }
+
+};
+
+class ScreenSpaceTriangle {
+    public:
+    PRIM_VERTEX vertices[3];
+    Mesh mesh;
+
+    // We may want to share this between multiple meshes later. Like a storage of Layouts
+    D3D12_INPUT_ELEMENT_DESC inputLayout[2];
+    D3D12_INPUT_LAYOUT_DESC inputLayoutDesc;
+
+    ScreenSpaceTriangle() {}
+
+    void init(Core* core) {
+        PRIM_VERTEX vertices[3];
+        vertices[0].position = Vec3(0, 1.0f, 0);
+        vertices[0].colour = Colour(0, 1.0f, 0);
+        vertices[1].position = Vec3(-1.0f, -1.0f, 0);
+        vertices[1].colour = Colour(1.0f, 0, 0);
+        vertices[2].position = Vec3(1.0f, -1.0f, 0);
+        vertices[2].colour = Colour(0, 0, 1.0f);
+
+        mesh.init(core, &vertices[0], sizeof(PRIM_VERTEX), 3);
+
+        inputLayout[0] = { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT,
+        D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
+        inputLayout[1] = { "COLOUR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT,
+        D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 };
+        inputLayoutDesc.NumElements = 2;
+        inputLayoutDesc.pInputElementDescs = inputLayout;
+    }
+};
+
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
     Window win;
