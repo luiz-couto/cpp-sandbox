5b9a59c679cc9b431bece4b3f1c3ba39823970a2
luiz-couto <luizcouto.cg@gmail.com>
2025-11-26 09:05:28 +0000

    Barrier and Fence

---
+++ gpu/Core.h
@@ -0,0 +1,246 @@
+#pragma once
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <vector>
+#include "Fence.h"
+#include "Barrier.h"
+
+#pragma comment(lib, "d3d12")
+#pragma comment(lib, "dxgi")
+#pragma comment(lib, "d3dcompiler.lib")
+
+class Core {
+public:
+    int wWidth, wHeight;
+    IDXGIAdapter1* adapter;
+
+    ID3D12Device5* device;
+    ID3D12CommandQueue* graphicsQueue;
+    ID3D12CommandQueue* copyQueue;
+    ID3D12CommandQueue* computeQueue;
+    IDXGISwapChain3* swapchain;
+    ID3D12CommandAllocator* graphicsCommandAllocator[2];
+    ID3D12GraphicsCommandList4* graphicsCommandList[2];
+    ID3D12DescriptorHeap* backbufferHeap;
+    ID3D12Resource** backbuffers;
+    ID3D12DescriptorHeap* dsvHeap;
+    ID3D12Resource* dsv;
+    D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle;
+    D3D12_VIEWPORT viewport;
+    D3D12_RECT scissorRect;
+
+
+    GPUFence graphicsQueueFence[2];
+
+    Core() {}
+
+    // finds the adapter with bigger maxVideoMemory and uses it
+    // creates the device
+    // creates the 3 command queues: graphics, copy and compute
+    // creates the swapchain
+    // creates 2 command allocators and command lists
+    // creates the backbuffer descriptor heap: 2 descriptors - one for each backbuffer
+    // creates the render target views for each backbuffer
+    // creates the fences for the graphics queue
+    // creates the depth stencil view and resource
+    // creates the dsv descriptor heap
+    // allocates the depth stencil resource in GPU local memory
+    // create viewport and scissor rect
+    void init(HWND hwnd, int _width, int _height) {
+        wWidth = _width;
+        wHeight = _height;
+
+        IDXGIAdapter1* adapterf;
+        std::vector<IDXGIAdapter1*> adapters;
+        IDXGIFactory6* factory = NULL;
+        CreateDXGIFactory(__uuidof(IDXGIFactory6), (void**)&factory);
+        int i = 0;
+
+        while (factory->EnumAdapters1(i, &adapterf) != DXGI_ERROR_NOT_FOUND) {
+            adapters.push_back(adapterf);
+            i++;
+        }
+
+        long long maxVideoMemory = 0;
+        int useAdapterIndex = 0;
+        for (int i = 0; i < adapters.size(); i++) {
+            DXGI_ADAPTER_DESC desc;
+            adapters[i]->GetDesc(&desc);
+            if (desc.DedicatedVideoMemory > maxVideoMemory) {
+                maxVideoMemory = desc.DedicatedVideoMemory;
+                useAdapterIndex = i;
+            }
+        }
+
+        adapter = adapters[useAdapterIndex];
+
+        D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&device));
+
+        D3D12_COMMAND_QUEUE_DESC graphicsQueueDesc = {};
+        graphicsQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+        device->CreateCommandQueue(&graphicsQueueDesc, IID_PPV_ARGS(&graphicsQueue));
+
+        D3D12_COMMAND_QUEUE_DESC copyQueueDesc = {};
+        copyQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_COPY;
+        device->CreateCommandQueue(&copyQueueDesc, IID_PPV_ARGS(&copyQueue));
+
+        D3D12_COMMAND_QUEUE_DESC computeQueueDesc = {};
+        computeQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_COMPUTE;
+        device->CreateCommandQueue(&computeQueueDesc, IID_PPV_ARGS(&computeQueue));
+
+        DXGI_SWAP_CHAIN_DESC1 scDesc = {};
+        scDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        scDesc.Width = wWidth;
+        scDesc.Height = wHeight;
+        scDesc.SampleDesc.Count = 1; // MSAA here
+        scDesc.SampleDesc.Quality = 0;
+        scDesc.BufferCount = 2;
+        scDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+        IDXGISwapChain1* swapChain1;
+        factory->CreateSwapChainForHwnd(graphicsQueue, hwnd, &scDesc, NULL, NULL, &swapChain1);
+        swapChain1->QueryInterface(&swapchain);
+        swapChain1->Release();
+
+        factory->Release();
+
+        device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+        IID_PPV_ARGS(&graphicsCommandAllocator[0]));
+        device->CreateCommandList1(0, D3D12_COMMAND_LIST_TYPE_DIRECT, D3D12_COMMAND_LIST_FLAG_NONE,
+        IID_PPV_ARGS(&graphicsCommandList[0]));
+    
+        device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+        IID_PPV_ARGS(&graphicsCommandAllocator[1]));
+        device->CreateCommandList1(0, D3D12_COMMAND_LIST_TYPE_DIRECT, D3D12_COMMAND_LIST_FLAG_NONE,
+        IID_PPV_ARGS(&graphicsCommandList[1]));
+
+        D3D12_DESCRIPTOR_HEAP_DESC renderTargetViewHeapDesc = {};
+        renderTargetViewHeapDesc.NumDescriptors = scDesc.BufferCount;
+        renderTargetViewHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        device->CreateDescriptorHeap(&renderTargetViewHeapDesc, IID_PPV_ARGS(&backbufferHeap));
+
+        backbuffers = new ID3D12Resource *[scDesc.BufferCount];
+
+        D3D12_CPU_DESCRIPTOR_HANDLE renderTargetViewHandle = backbufferHeap->GetCPUDescriptorHandleForHeapStart();
+        unsigned int renderTargetViewDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV); // size of one descriptor
+        for (unsigned int i = 0; i < 2; i++) {
+            swapchain->GetBuffer(i, IID_PPV_ARGS(&backbuffers[i]));
+            device->CreateRenderTargetView(backbuffers[i], nullptr, renderTargetViewHandle);
+            renderTargetViewHandle.ptr += renderTargetViewDescriptorSize;
+        }
+
+        graphicsQueueFence[0].create(device);
+        graphicsQueueFence[1].create(device);
+
+        D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc;
+        memset(&dsvHeapDesc, 0, sizeof(D3D12_DESCRIPTOR_HEAP_DESC));
+        dsvHeapDesc.NumDescriptors = 1;
+        dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+        dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        device->CreateDescriptorHeap(&dsvHeapDesc, IID_PPV_ARGS(&dsvHeap));
+        dsvHandle = dsvHeap->GetCPUDescriptorHandleForHeapStart();
+
+        D3D12_DEPTH_STENCIL_VIEW_DESC depthStencilDesc = {};
+        depthStencilDesc.Format = DXGI_FORMAT_D32_FLOAT;
+        depthStencilDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+        depthStencilDesc.Flags = D3D12_DSV_FLAG_NONE;
+        D3D12_CLEAR_VALUE depthClearValue = {};
+        depthClearValue.Format = DXGI_FORMAT_D32_FLOAT;
+        depthClearValue.DepthStencil.Depth = 1.0f;
+        depthClearValue.DepthStencil.Stencil = 0;
+
+        D3D12_HEAP_PROPERTIES heapprops = {};
+        heapprops.Type = D3D12_HEAP_TYPE_DEFAULT; // GPU local memory
+        heapprops.CreationNodeMask = 1;
+        heapprops.VisibleNodeMask = 1;
+
+        D3D12_RESOURCE_DESC dsvDesc = {};
+        dsvDesc.Format = DXGI_FORMAT_D32_FLOAT;
+        dsvDesc.Width = wWidth;
+        dsvDesc.Height = wHeight;
+        dsvDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+        dsvDesc.DepthOrArraySize = 1;
+        dsvDesc.MipLevels = 1;
+        dsvDesc.SampleDesc.Count = 1;
+        dsvDesc.SampleDesc.Quality = 0;
+        dsvDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+        dsvDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+
+        device->CreateCommittedResource(
+            &heapprops, D3D12_HEAP_FLAG_NONE,
+            &dsvDesc, D3D12_RESOURCE_STATE_DEPTH_WRITE,
+            &depthClearValue, IID_PPV_ARGS(&dsv)
+        );
+
+        device->CreateDepthStencilView(dsv, &depthStencilDesc, dsvHandle);
+
+        viewport.TopLeftX = 0.0f;
+        viewport.TopLeftY = 0.0f;
+        viewport.Width = (float)wWidth;
+        viewport.Height = (float)wHeight;
+        viewport.MinDepth = 0.0f;
+        viewport.MaxDepth = 1.0f;
+
+        scissorRect.left = 0;
+        scissorRect.top = 0;
+        scissorRect.right = wWidth;
+        scissorRect.bottom = wHeight;
+
+    }
+
+    void resetCommandList() {
+        unsigned int frameIndex = swapchain->GetCurrentBackBufferIndex(); // 0 or 1 depending on which buffer is being used
+        graphicsCommandAllocator[frameIndex]->Reset();
+        graphicsCommandList[frameIndex]->Reset(graphicsCommandAllocator[frameIndex], NULL);
+    }
+
+    ID3D12GraphicsCommandList4* getCommandList() {
+        unsigned int frameIndex = swapchain->GetCurrentBackBufferIndex();
+        return graphicsCommandList[frameIndex];
+    }
+
+    void runCommandList() {
+        getCommandList()->Close();
+        ID3D12CommandList* lists[] = { getCommandList() }; // executing one list at time to make it simple for now
+        graphicsQueue->ExecuteCommandLists(1, lists);
+    }
+
+    void flushGraphicsQueue() {
+        for (int i = 0; i < 2; i++) {
+            graphicsQueueFence[i].signal(graphicsQueue);
+            graphicsQueueFence[i].wait();
+        }
+    }
+
+    void beginFrame() {
+        unsigned int frameIndex = swapchain->GetCurrentBackBufferIndex();
+        graphicsQueueFence[frameIndex].wait();
+
+        D3D12_CPU_DESCRIPTOR_HANDLE renderTargetViewHandle = backbufferHeap->GetCPUDescriptorHandleForHeapStart();
+        unsigned int renderTargetViewDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+        renderTargetViewHandle.ptr += frameIndex * renderTargetViewDescriptorSize;
+
+        Barrier::add(backbuffers[frameIndex], D3D12_RESOURCE_STATE_PRESENT,
+        D3D12_RESOURCE_STATE_RENDER_TARGET, getCommandList());
+
+        getCommandList()->OMSetRenderTargets(1, &renderTargetViewHandle, FALSE, &dsvHandle);
+        float color[4];
+        color[0] = 0;
+        color[1] = 0;
+        color[2] = 1.0;
+        color[3] = 1.0;
+        getCommandList()->ClearRenderTargetView(renderTargetViewHandle, color, 0, NULL);
+        getCommandList()->ClearDepthStencilView(dsvHandle, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, NULL);
+    }
+
+    void finishFrame() {
+        unsigned int frameIndex = swapchain->GetCurrentBackBufferIndex();
+        Barrier::add(backbuffers[frameIndex], D3D12_RESOURCE_STATE_RENDER_TARGET,
+        D3D12_RESOURCE_STATE_PRESENT, getCommandList());
+        runCommandList();
+        graphicsQueueFence[frameIndex].signal(graphicsQueue);
+        swapchain->Present(1, 0);
+    }
+
+};
--- gpu/gpu.cpp
+++ gpu/gpu.cpp
@@ -1,11 +1,22 @@
 #include "Window.h"
+#include "Core.h"
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
     Window win;
+    Core core;
     win.init(1024, 1024, 0, 0, "My Window");
-    while (true)
-    {
+    core.init(win.hwnd, win.windowWidth, win.windowHeight);
+
+    while (true) {
+        core.resetCommandList();
+        core.beginFrame();
         win.processMessages();
+        if (win.keys[VK_ESCAPE] == 1) {
+            break;
+        }
+        core.finishFrame();
     }
+
+    core.flushGraphicsQueue();
     return 0;
 }
