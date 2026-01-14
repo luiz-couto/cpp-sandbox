5b3633f9d29c53a546137f279a2c134281c146c0
luiz-couto <luizcouto.cg@gmail.com>
2026-01-14 11:08:29 +0000

    avx

---
+++ avx/avx.cpp
@@ -0,0 +1,66 @@
+#include <immintrin.h>
+#include <print>
+
+void testSupport() {
+    int cpu_info[4];
+    __cpuid(cpu_info, 7);
+
+    bool avx512_supported = (cpu_info[1] & (1 << 16)) != 0;
+    bool avx2_supported = (cpu_info[1] & (1 << 5)) != 0;
+
+    std::print("AVX-512 supported: {}\n", avx512_supported ? "Yes" : "No");
+    std::print("AVX2 supported: {}\n", avx2_supported ? "Yes" : "No");
+}
+
+void add_avx2(float* a, float* b, float* result) {
+    // load 8 floats from each array
+    __m256 va = _mm256_load_ps(a);
+    __m256 vb = _mm256_load_ps(b);
+
+    __m256 vc = _mm256_add_ps(va, vb);
+    _mm256_store_ps(result, vc);
+}
+
+void testAVX2() {
+    alignas(64) float a[8] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f};
+    alignas(64) float b[8] = {10.0f, 20.0f, 30.0f, 40.0f, 50.0f, 60.0f, 70.0f, 80.0f};
+    alignas(64) float result[8];
+
+    add_avx2(a, b, result);
+
+    std::print("Result of AVX2 addition:\n");
+    for (int i = 0; i < 8; ++i) {
+        std::print("{} ", result[i]);
+    }
+    std::print("\n");
+}
+
+void add_avx2_doubles(double* a, double* b, double* result) {
+    // load 4 doubles from each array
+    __m256d va = _mm256_load_pd(a);
+    __m256d vb = _mm256_load_pd(b);
+
+    __m256d vc = _mm256_add_pd(va, vb);
+    _mm256_store_pd(result, vc);
+}
+
+void testAVX2Doubles() {
+    alignas(64) double a[4] = {1.0, 2.0, 3.0, 4.0};
+    alignas(64) double b[4] = {10.0, 20.0, 30.0, 40.0};
+    alignas(64) double result[4];
+
+    add_avx2_doubles(a, b, result);
+
+    std::print("Result of AVX2 double addition:\n");
+    for (int i = 0; i < 4; ++i) {
+        std::print("{} ", result[i]);
+    }
+    std::print("\n");
+}
+
+int main() {
+    testSupport();
+    testAVX2();
+    testAVX2Doubles();
+    return 0;
+}
