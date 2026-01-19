ddc0ba816bcba2e783e3f5be803691e25446b49d
luiz-couto <luizcouto.cg@gmail.com>
2026-01-19 10:10:03 +0000

    launch jthreads

--- threads/threads.cpp
+++ threads/threads.cpp
@@ -12,22 +12,19 @@ void helloThread(int id) {
 
 int main() {
     // get number of CPUs – these include hyperthreaded
-    unsigned int numCPUs = std::thread::hardware_concurrency();
+    unsigned int numCPUs = std::jthread::hardware_concurrency();
     std::cout << "Number of CPUs is " << numCPUs << std::endl;
 
-    std::vector<std::thread> threads(numCPUs);
+    std::vector<std::jthread> threads(numCPUs);
 
-    // launch threads
-    for (int i = 0; i < threads.size(); i++)
-        threads[i] = std::thread([&i] { 
+    // launch jthreads
+    for (int i = 0; i < threads.size(); i++) {
+        threads[i] = std::jthread([&i] { 
             std::string threadID = std::format("{}", std::this_thread::get_id());
             std::string message = "Hello world! I am thread " + std::to_string(i) + " ; my id is " + threadID + "\n";
             std::cout << message;
         });
-
-    // wait for threads to finish
-    for (auto& t : threads)
-        t.join();
+    }
     
     return 0;
 }
